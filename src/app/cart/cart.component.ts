a2a6697b43ef55a86ac5b86cd5dd0bb6e141a5db
David Gómez <gomez.david.89@gmail.com>
2020-02-28 17:52:26 -0800

    Integrate the cart service with the cart component.

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CartService } from '../cart.service';
+import { Product } from '../product';
+
 @Component({
   selector: 'app-cart',
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
 export class CartComponent implements OnInit {
+  products: Product[];
 
-  constructor() { }
+  constructor(private cartService: CartService) { }
 
   ngOnInit(): void {
+    this.products = this.cartService.getItems();
   }
-
 }
--- src/app/product.ts
+++ src/app/product.ts
@@ -1,3 +1,5 @@
+// TODO: promote Product to a class with a getPrice method that returns a
+// string of the price in the local currency.
 export interface Product {
   name: string;
   price: number;
